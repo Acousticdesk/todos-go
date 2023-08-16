f9ac9be856166c39efec0fabe8a82c43e9fe4f85
Andrii Kicha <akicha@star.global>
2023-08-16 09:48:58 -0600

    fix issue with trimming the end line while printing todos & create gitignore

--- todos-main.go
+++ todos-main.go
@@ -2,13 +2,15 @@ package main
 
 import "strings"
 
+// import "strings"
+
 func todosToString(todos []string) string {
 	todosContent := ""
 	for i := 0; i < len(todos); i ++ {
 		todosContent += todos[i]
 		todosContent += "\n\r"
 	}
-	return strings.Replace(todosContent, "\n\r", "", -1)
+	return strings.TrimSuffix(todosContent, "\n\r")
 }
 
 const HelpCommand = "help"
