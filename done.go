61f81ab891c1ffd1ffe144d43149ee640c7b5513
Andrii Kicha <akicha@star.global>
2023-08-17 11:10:46 -0600

    fix issue with the done command when selected index does not exist

--- done.go
+++ done.go
@@ -16,12 +16,19 @@ func doneCommand(todos []string) []string {
 	fmt.Println("Which one should be marked as done?")
 
 	scanner.Scan()
+	
 	selectedOrder, err := strconv.Atoi(scanner.Text())
 	if (err != nil) {
 		fmt.Println("Couldn't find the todo with this number")
 		return todos
 	}
+	
 	selectedIndex := selectedOrder - 1
+	
+	if (selectedIndex < 0 || selectedIndex >= len(todos)) {
+		fmt.Println("Couldn't find the todo with this number")
+		return todos
+	}
 
 	todos = append(todos[:selectedIndex], todos[selectedIndex + 1])
 
